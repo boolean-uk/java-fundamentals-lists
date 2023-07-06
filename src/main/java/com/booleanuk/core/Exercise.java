c9f7300b3072ba6acc7cbf9600b8d973211cfa6a
Anastasiya <ronas0709@gmail.com>
2023-07-06 13:08:03 +0200

    solve 4-6

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -79,6 +79,10 @@ public class Exercise extends ExerciseBase {
          - A string
          The method must add the second parameter into the list provided and then return the list
      */
+    public ArrayList<String> addIngredient(ArrayList<String> list, String ingredient){
+        list.add(ingredient);
+        return list;
+    }
 
 
 
@@ -88,6 +92,11 @@ public class Exercise extends ExerciseBase {
          - A string
          The method must remove the second parameter from the list and then return the list
      */
+    public ArrayList<String> removeIngredient(ArrayList<String> list, String ingredient){
+        list.remove(ingredient);
+        return list;
+    }
+
 
 
 
@@ -97,6 +106,11 @@ public class Exercise extends ExerciseBase {
          - A string
          The method must return a boolean that indicates whether the second parameter exists in the provided list
      */
+    public  boolean  containsIngredient(ArrayList<String> list, String ingredient){
+        return list.contains(ingredient);
+    }
+
+
 
 
 
