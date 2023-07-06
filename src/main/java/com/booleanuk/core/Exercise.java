253e5f8e8d39162cf29465fbf14a7ccd3156abcd
Rafal Halama <rafau.halama@gmail.com>
2023-07-06 12:58:06 +0200

    first 3 tests

--- src/main/java/com/booleanuk/core/Exercise.java
+++ src/main/java/com/booleanuk/core/Exercise.java
@@ -43,7 +43,9 @@ public class Exercise extends ExerciseBase {
         TODO: 1. Create a method named getSecondNumber that returns a whole number. It must return the
            second number contained in the list that is returned from getFavouriteNumbers
      */
-
+        public int getSecondNumber(){
+            return getFavouriteNumbers().get(1);
+        }
 
 
     /*
@@ -56,7 +58,10 @@ public class Exercise extends ExerciseBase {
          https://www.programiz.com/java-programming/library/arraylist/replaceall
      */
 
-
+        public ArrayList<Integer> multiply(ArrayList<Integer> arr, int num){
+            arr.replaceAll(x-> x * num);
+            return arr;
+        }
 
     /*
         TODO: 3. Create a method named isEmpty that accepts one parameter:
@@ -64,7 +69,10 @@ public class Exercise extends ExerciseBase {
          The method must return a boolean that indicates whether the provided list is empty or not
      */
 
+        public boolean isEmpty(ArrayList<String> list){
+            return list.isEmpty();
 
+        }
 
     /*
         TODO: 4. Create a method named addIngredient that accepts two parameters in this order:
